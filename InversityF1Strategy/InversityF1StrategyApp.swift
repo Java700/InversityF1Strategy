7408a52a4e730bd529d3fb49a0f5d4441203cb8a
Jack Turner <jack.turner123@icloud.com>
2024-03-02 10:15:26 +0000

    Initial Commit

---
+++ InversityF1Strategy/InversityF1StrategyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InversityF1StrategyApp.swift
+//  InversityF1Strategy
+//
+//  Created by Jack Turner on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct InversityF1StrategyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
